5ea4e0fe72a01e32e22bf1849ffc4fbb5df51d62
Pedro Vinicius <139086553+Pedro-2077@users.noreply.github.com>
2024-12-14 23:42:21 +0000

    atualizando o ultimo commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-return <h1>Te amo minha JUJUBA LINDA, quero voce para sempre para toda minha vida</h1>
+return <h1>Te amo minha JUJUBA LINDA, quero voce para sempre para toda minha vida,Você é a melhor do MUNDO ERICA ALMEIDA C</h1>
 }
 
 export default Home;
