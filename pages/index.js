a9a3e7c99d0b3f8770960152316ceeb481bcf26f
Pedro Vinicius <139086553+Pedro-2077@users.noreply.github.com>
2024-12-18 03:22:19 +0000

    adicionando scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
-function Home(){
-return <h1>Te amo minha jujuba linda, quero você para sempre para toda minha vida, Erica você é a melhor do MUNDO</h1>
+function Home() {
+  return (
+    <h1>
+      Te amo minha jujuba linda, quero você para sempre para toda minha vida,
+      Erica você é a melhor do MUNDO
+    </h1>
+  );
+}
+
+function teste02() {
+  console.log("teste");
 }
 
 export default Home;
